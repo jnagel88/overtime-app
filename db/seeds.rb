291f51659496a3282283d0f98f407384bf7be56a
Joe Nagel <joe@cratebind.com>
2018-06-29 16:52:27 -0500

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,16 @@
+
+User.destroy_all
+puts "Destroyed all users"
+Post.destroy_all
+puts "Destroyed all posts"
+
+
 User.create!(email:'test@test.com', password:'test123', password_confirmation: 'test123', first_name: 'Jon', last_name:'Snow')
 
 puts "test user created."
 
 100.times do |post|
-	Post.create!(date: Date.today, rationale: "rationale #{post}")
+	Post.create!(date: Date.today, rationale: "rationale #{post}", user_id: User.last.id)
 	
 end
 puts "100 posts have been created."
